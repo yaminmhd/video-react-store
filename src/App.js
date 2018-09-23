ee3e52927d258972dc9944f102d2d900a6a0a8d1
yaminmhd <mdyamin.drzee@gmail.com>
2018-09-23 12:52:09 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import Movies from "./Movies";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Movies/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movies.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import MovieItem from "./MovieItem";
+import { getMovies } from "./services/fakeMovieService";
+
+class Movies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: getMovies()
+    };
+  }
+
+  deleteMovie(id){
+    const { movies } = this.state;
+    const filteredMovies = movies.filter(movie => movie._id !== id);
+    this.setState({
+      movies: filteredMovies
+    });
+  }
+
+  render() {
+    const { movies } = this.state;
+    const headers = (
+      <thead>
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Stock</th>
+          <th scope="col">Rate</th>
+          <th scope="col" />
+        </tr>
+      </thead>
+    );
+    const checkMoviesLength =
+      movies.length === 0
+        ? `There are no movies in the database`
+        : `Showing ${movies.length} movies in the database`;
+    return (
+      <div>
+        <h3 className="m-2">{checkMoviesLength}</h3>
+        <table className="table">
+          {movies.length !== 0 ? headers : <thead></thead>}
+          <tbody>
+            {movies.map(movie => {
+              return (
+                <MovieItem
+                  key={movie._id}
+                  movie={movie}
+                  deleteHandler={() => this.deleteMovie(movie._id)}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
