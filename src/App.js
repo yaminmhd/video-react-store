c8fe8a691c31b383a110ebe5b04aead572aec372
yaminmhd <mdyamin.drzee@gmail.com>
2018-09-23 12:58:16 +0800

    edited master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Movies from "./Movies";
 
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Movies/>
       </div>
     );
   }
